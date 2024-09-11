d7007fe2c82e654978d332898d7efef8c149dd20
Fabiano Avelar <fabianoavelar86@gmail.com>
2024-09-11 00:14:03 -0300

    Incluido projeto com ANTL

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AvelarLanguage
-Trabalho de Compiladores UFABC
+## Trabalho de Compiladores UFABC
+
+Defina a sua própria gramática e os tokens necessários. Os requisitos mínimos são os de uma calculadora simples, que:
+
+- Recebe como entrada variáveis, parênteses, operadores e números reais.
+- Dá como resultado uma mensagem de erro explicativa ou o valor calculado.
---
+++ src/br/com/ufabc/avelarlanguage/main/MainClass.java
@@ -0,0 +1,38 @@
+package br.com.ufabc.avelarlanguage.main;
+
+import br.com.ufabc.avelarlanguage.parser.AvelarlangLexer;
+import br.com.ufabc.avelarlanguage.parser.AvelarlangParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+/* esta é a classe que é responsável por criar a interação com o usuário
+ * instanciando nosso parser e apontando para o arquivo fonte
+ *
+ * Arquivo fonte: extensao .avl
+ *
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        try {
+            AvelarlangLexer lexer;
+            AvelarlangParser parser;
+
+            // leio o arquivo "input.isi" e isso é entrada para o Analisador Lexico
+            lexer = new AvelarlangLexer(CharStreams.fromFileName("input.avl"));
+
+            // crio um "fluxo de tokens" para passar para o PARSER
+            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+
+            // crio meu parser a partir desse tokenStream
+            parser = new AvelarlangParser(tokenStream);
+
+            parser.prog();
+
+            System.out.println("Compilation Successful");
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+            System.err.println("ERROR "+ex.getMessage());
+        }
+    }
+}
